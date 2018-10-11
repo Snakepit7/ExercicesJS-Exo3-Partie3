b179a18b82a13368c71dc6984b286c7dea1e6e09
Snakepit7 <julienfollet71@gmail.com>
2018-10-11 14:03:54 +0200

    Fin

---
+++ index.js
@@ -0,0 +1,11 @@
+document.getElementById("btn1").addEventListener("click", function() {
+  text.style.color="green";
+})
+
+document.getElementById("btn2").addEventListener("click", function() {
+  text.style.color="red";
+})
+
+document.getElementById("btn3").addEventListener("click", function() {
+  text.style.color="blue";
+})
